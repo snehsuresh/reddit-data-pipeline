21c08b3d5c2e41c09b348d31bcd4033ca673d727
snehsuresh <snehsuresh002@gmail.com>
2024-07-21 17:28:31 -0500

    s3 integration

--- dags/reddit_dag.py
+++ dags/reddit_dag.py
@@ -13,7 +13,7 @@ If you are running Docker and using Airflow in your Docker instance, you might e
 '''
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# from pipelines.aws_s3_pipeline import upload_s3_pipeline
+from pipelines.aws_s3_pipeline import upload_s3_pipeline
 from pipelines.reddit_pipeline import reddit_pipeline
 
 default_args = {
@@ -45,10 +45,10 @@ extract = PythonOperator(
 )
 
 # upload to s3
-# upload_s3 = PythonOperator(
-#     task_id='s3_upload',
-#     python_callable=upload_s3_pipeline,
-#     dag=dag
-# )
+upload_s3 = PythonOperator(
+    task_id='s3_upload',
+    python_callable=upload_s3_pipeline,
+    dag=dag
+)
 
 # extract >> upload_s3
